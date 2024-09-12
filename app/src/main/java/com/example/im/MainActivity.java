a26df10438cab1837f35751f4bbd17d13d0a1838
Exkyu <joaquinandesc@gmail.com>
2024-09-12 00:57:11 -0300

    Primeros botones funcionando

--- app/src/main/java/com/example/im/MainActivity.java
+++ app/src/main/java/com/example/im/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.im;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnLogin;
+    private Button btnShowMap;
+    private Button btnOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnLogin = (Button) findViewById(R.id.idLogin);
+        btnShowMap = (Button) findViewById(R.id.idShowMap);
+        btnOut = (Button) findViewById(R.id.idOut);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conectL();
+            }
         });
+
+        btnOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void conectL(){
+        Intent intent = new Intent(this,Login.class);
+        startActivity(intent);
     }
 }
